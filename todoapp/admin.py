91aae06d93de88e3a92f2de2bc85dcbdf1bc05e0
fercontreras10 <90367308+fercontreras10@users.noreply.github.com>
2024-05-09 02:15:55 -0400

    css

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from todoapp.models import  User, Tarea
+#from categorias.models import Categoria
 
+#admin.site.register(Categoria)
 admin.site.register(User)
 admin.site.register(Tarea)
 
